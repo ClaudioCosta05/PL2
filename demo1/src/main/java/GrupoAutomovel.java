3f1dfcf87df7a1c4e0a9e9871fc7474f859b9741
Cláudio Daniel Cruz Costa <1230870@isep.ipp.pt>
2025-03-18 15:21:22 +0000

    .

--- demo1/src/main/java/GrupoAutomovel.java
+++ demo1/src/main/java/GrupoAutomovel.java
@@ -1,11 +1,32 @@
-public class GrupoAutomovel {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class GrupoAutomovel implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
     private int portas;
     private String classe;
-
     public GrupoAutomovel(String nome, int portas, String classe) {
         this.nome = nome;
         this.portas = portas;
         this.classe = classe;
+    }
+
+    public GrupoAutomovel() {
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
